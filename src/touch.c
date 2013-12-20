2153b1a995e2c36c7bdba9c04f6bcaf036598a63
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-12-20 11:42:51 -0500

    Do touch

--- src/touch.c
+++ src/touch.c
@@ -1,1 +1,16 @@
 #include "touch.h"
+
+#include <stdio.h>
+#include <sys/time.h>
+
+#include <multilib/io.h>
+
+int touch(char *filename) {
+	// note: on the following line, the mode appears to be ignored, even when creating a new file.
+	// I'm not sure why this is, but it doesn't really matter.
+	ml_fd_t fd = ml_iofopen(filename, O_RDWR | O_CREAT | O_APPEND, ml_iostr2mod("rw-rw-rw-"));
+	if (futimes(fd, NULL) != 0) {
+		fprintf(stderr, "We errored :(\n");
+	}
+	ml_iofclose(fd);
+}
