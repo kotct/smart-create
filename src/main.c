63ac56052605985e287a3ba6a9abdd1099cc9be8
Sammidysam <sammidysam@gmail.com>
2013-12-20 12:41:04 -0500

    Comments

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@ int main(int argc, char *argv[])
 	/* Create arguments and files to be made list. */
 	ml_argument_list_t arguments = ml_arglstgen(argc, argv);
 	ml_list_t *files_to_be_made = NULL;
-	
+
+	/* Scan arguments, add arguments to files_to_be_made. */
 	for (int i = 1; i < argc; i++) {
 		char *argument_data = ml_arglstgnth(arguments, i)->data;
 		
@@ -43,6 +44,7 @@ int main(int argc, char *argv[])
 			files_to_be_made = ml_lnnew(files_to_be_made, argument_data);
 	}
 
+	/* Create the files that must be made. */
 	for (size_t i = 0; i < ml_lgnn(files_to_be_made); i++) {
 		touch(ml_lngdat(ml_lgnth(files_to_be_made, i)));
 	}
