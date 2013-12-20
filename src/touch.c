fdedc713608c26974d2d994feda9f96df58ec4ec
Christopher Cooper <christopher.c.cooper@gmail.com>
2013-12-20 11:49:51 -0500

    Slightly change touch, now done.

--- src/touch.c
+++ src/touch.c
@@ -9,8 +9,6 @@ int touch(char *filename) {
 	// note: on the following line, the mode appears to be ignored, even when creating a new file.
 	// I'm not sure why this is, but it doesn't really matter.
 	ml_fd_t fd = ml_iofopen(filename, O_RDWR | O_CREAT | O_APPEND, ml_iostr2mod("rw-rw-rw-"));
-	if (futimes(fd, NULL) != 0) {
-		fprintf(stderr, "We errored :(\n");
-	}
 	ml_iofclose(fd);
+	utimes(filename, NULL);
 }
