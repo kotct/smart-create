7ea5df1128e37016b4c93f79a61de29dd5caca31
Kristofer Rye <kristofer.rye@gmail.com>
2013-12-20 11:54:16 -0500

    Move the touch call and touch every file

--- src/main.c
+++ src/main.c
@@ -20,12 +20,8 @@ int main(int argc, char *argv[])
 	ml_argument_list_t arguments = ml_arglstgen(argc, argv);
 
 	for (int i = 1; i < argc; i++) {
-		printf("%s\n", ml_argument_list_get_nth(arguments, i)->data);
+		touch(ml_argument_list_get_nth(arguments, i)->data);
 	}
 
-	if (argc > 1) {
-		touch(ml_argument_list_get_nth(arguments, 1)->data);
-	}
-	
 	return 0;
 }
