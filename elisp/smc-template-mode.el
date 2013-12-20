b4bc9ff15c9fc2bf513f91e11e79dcf8c61b6949
Sammidysam <sammidysam@gmail.com>
2013-12-20 09:22:12 -0500

    Template mode file

---
+++ elisp/smc-template-mode.el
@@ -0,0 +1,30 @@
+;;; smc-template-mode.el --- Emacs Major Mode for Smart Create Template files
+
+;; Copyright (C) 2013 Sam Craig, Kristofer Rye, Christopher Cooper
+
+;; Author: Sam Craig <sammidysam@gmail.com>, Kristofer Rye <kristofer.rye@gmail.com>, Christopher Cooper <christopher.c.cooper@gmail.com>
+;; Keywords: Smart Create, Template
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+
+
+(provide 'smc-template-mode)
+;;; smc-template-mode.el ends here
